a919a2c579516089e2ef8c7e56cc15180e1b3221
Gokul2172001 <gokul.elumalai12@gmail.com>
2024-07-27 23:30:27 +0530

    Code Improvements and build fixes

--- src/Basics.java
+++ src/Basics.java
@@ -4,13 +4,13 @@ import static io.restassured.RestAssured.given;
 import io.restassured.RestAssured;
 //import io.restassured.RestAssured;
 
-public class basic {
+public class Basics {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
 		RestAssured.baseURI = "https://rahulshettyacademy.com";
-		given().queryParam("key", "qaclick123").header("Content-Type", "application/json")
+		given().log().all().queryParam("key", "qaclick123").header("Content-Type", "application/json")
 				.body("{\r\n" + "    \"location\": {\r\n" + "        \"lat\": -38.383494,\r\n"
 						+ "        \"lng\": 33.427362\r\n" + "    },\r\n" + "    \"accuracy\": 50,\r\n"
 						+ "    \"name\": \"Rahul Shetty Academy\",\r\n"
@@ -19,7 +19,8 @@ public class basic {
 						+ "        \"shoe park\",\r\n" + "        \"shop\"\r\n" + "    ],\r\n"
 						+ "    \"website\": \"https://rahulshettyacademy.com\",\r\n"
 						+ "    \"language\": \"French-IN\"\r\n" + "    }")
-				.when().post("maps/api/place/add/json").then().assertThat().statusCode(200);
+				.when().post("maps/api/place/add/json").then().log().all().statusCode(200);
+		System.out.println("Just run");
 	}
 
 }
