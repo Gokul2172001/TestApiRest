b63bf0f27312dc2e2c61567164be10cae0b265b3
Gokul2172001 <gokul.elumalai12@gmail.com>
2024-08-04 23:32:29 +0530

    updates on json body from different function

--- src/Basics.java
+++ src/Basics.java
@@ -3,6 +3,7 @@ import static io.restassured.RestAssured.given;
 //import io.restassured.RestAssured;
 import static org.hamcrest.Matchers.equalTo;
 
+import files.payload;
 import io.restassured.RestAssured;
 
 public class Basics {
@@ -10,18 +11,10 @@ public class Basics {
 	public static void main(String[] args) {
 
 		RestAssured.baseURI = "https://rahulshettyacademy.com";
-		
+
 		given().log().all().queryParam("key", "qaclick123").header("Content-Type", "application/json")
-				.body("{\r\n" + "    \"location\": {\r\n" + "        \"lat\": -38.383494,\r\n"
-						+ "        \"lng\": 33.427362\r\n" + "    },\r\n" + "    \"accuracy\": 50,\r\n"
-						+ "    \"name\": \"Rahul Shetty Academy\",\r\n"
-						+ "    \"phone_number\": \"(+91) 983 893 3937\",\r\n"
-						+ "    \"address\": \"29, side layout, cohen 09\",\r\n" + "    \"types\" : [\r\n"
-						+ "        \"shoe park\",\r\n" + "        \"shop\"\r\n" + "    ],\r\n"
-						+ "    \"website\": \"https://rahulshettyacademy.com\",\r\n"
-						+ "    \"language\": \"French-IN\"\r\n" + "    }")
-				.when().post("maps/api/place/add/json").then().log().all().statusCode(200).body("scope", equalTo("APP"))
-				.header("Server", "Apache/2.4.52 (Ubuntu)");
+				.body(payload.AddPlace()).when().post("maps/api/place/add/json").then().log().all().statusCode(200)
+				.body("scope", equalTo("APP")).header("Server", "Apache/2.4.52 (Ubuntu)");
 	}
 
 }
