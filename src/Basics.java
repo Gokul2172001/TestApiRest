4815e1a50d619e89b49520c890f656896fc66b4c
Gokul2172001 <gokul.elumalai12@gmail.com>
2024-08-06 22:53:09 +0530

    New Changes with updations

--- src/Basics.java
+++ src/Basics.java
@@ -5,6 +5,7 @@ import static org.hamcrest.Matchers.equalTo;
 
 import files.payload;
 import io.restassured.RestAssured;
+import io.restassured.path.json.JsonPath;
 
 public class Basics {
 
@@ -12,9 +13,15 @@ public class Basics {
 
 		RestAssured.baseURI = "https://rahulshettyacademy.com";
 
-		given().log().all().queryParam("key", "qaclick123").header("Content-Type", "application/json")
-				.body(payload.AddPlace()).when().post("maps/api/place/add/json").then().log().all().statusCode(200)
-				.body("scope", equalTo("APP")).header("Server", "Apache/2.4.52 (Ubuntu)");
+		String response = given().queryParam("key", "qaclick123").header("Content-Type", "application/json")
+				.body(payload.AddPlace()).when().post("maps/api/place/add/json").then().statusCode(200)
+				.body("scope", equalTo("APP")).header("Server", "Apache/2.4.52 (Ubuntu)").extract().response()
+				.asString();
+		System.out.println(response);
+		JsonPath js = new JsonPath(response);
+		
+		String placeId = js.getString("place_id");
+		System.out.println(placeId);
 	}
 
 }
