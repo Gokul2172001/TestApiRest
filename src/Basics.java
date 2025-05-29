22358436365a7af1a3844c326518869543a59b18
Gokul E <76160646+Gokul2172001@users.noreply.github.com>
2025-05-29 23:48:19 +0530

    Update Basics.java

--- src/Basics.java
+++ src/Basics.java
@@ -18,17 +18,16 @@ public class Basics {
 				.asString();
 
 		System.out.println(response);
+		
 		JsonPath js = new JsonPath(response);
 
 		String placeId = js.getString("place_id");
 		System.out.println(placeId);
 
-		
 		given().queryParam("key", "qaclick123").header("Content-Type", "application/json").body(payload.AddPlace())
 				.when().post("maps/api/place/add/json").then().statusCode(200).body("scope", equalTo("APP"))
 				.header("Server", "Apache/2.4.52 (Ubuntu)").extract().response().asString();
 		
-
 		System.out.println(placeId);
 
 	}
