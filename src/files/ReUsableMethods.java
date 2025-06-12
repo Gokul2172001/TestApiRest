7aeb370d3ad7f2e35847e89d807b7c66f2e8a109
Gokul E <76160646+Gokul2172001@users.noreply.github.com>
2025-06-12 23:53:32 +0530

    Update ReUsableMethods.java

--- src/files/ReUsableMethods.java
+++ src/files/ReUsableMethods.java
@@ -3,11 +3,8 @@ import io.restassured.path.json.JsonPath;
 
 public class ReUsableMethods {
 
-	public static JsonPath rawToJson(String response)
-	
-	{
+	public static JsonPath rawToJson(String response)	{
 		JsonPath js1 =new JsonPath(response);
-		
 		return js1;
 	}
 }
