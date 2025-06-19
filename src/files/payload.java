0bc1f9213b507a27e97ac218e063cc898810c131
Gokul E <76160646+Gokul2172001@users.noreply.github.com>
2025-06-19 23:02:44 +0530

    Update payload.java

--- src/files/payload.java
+++ src/files/payload.java
@@ -2,8 +2,7 @@ package files;
 
 public class payload {
 
-	public static String AddPlace()
-	{
+	public static String AddPlace() {
 		return "{\r\n" + "  \"location\": {\r\n" + "    \"lat\": -38.383494,\r\n" + "    \"lng\": 33.427362\r\n"
 				+ "  },\r\n" + "  \"accuracy\": 50,\r\n" + "  \"name\": \"Rahul Shetty Academy\",\r\n"
 				+ "  \"phone_number\": \"(+91) 983 893 3937\",\r\n"
